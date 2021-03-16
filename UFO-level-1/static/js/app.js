e192896ca6080e27688a44f6e518d75a724b80ff
Franz365 <74512752+Franz365@users.noreply.github.com>
2021-03-16 18:05:08 +1100

    Finished Ufo 1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,58 @@
-// from data.js
+// Assign the data from `data.js` to a descriptive variable
 var tableData = data;
 
-// YOUR CODE HERE!
+// Select the table body
+var tbody = d3.select("tbody");
+
+// Select the button
+var button = d3.select("#filter-btn");
+
+// Iterate through the data and create rows and cells, and log out the 
+tableData.forEach(function(ufodata){
+    console.log(ufodata);
+    var row=tbody.append("tr");
+    Object.entries(ufodata).forEach(function([key, value]){
+        var cell=row.append("td");
+        cell.text(value);
+    });
+});
+
+// Create event handlers
+button.on("click",runenter);
+button.on("submit",runenter);
+
+// Complete the event handler function for the form
+function runenter(){
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Clear all data from previous searches
+    tbody.html("");
+
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+
+    // Get the value property of the input element
+    var inputDate = inputElement.property("value");
+
+    filterdata=tableData.filter(filterValue => filterValue.datetime===inputDate);
+    
+    //remove anything from the list
+    tbody.html("");
+
+    // log the filtered values
+    console.log(filterdata);
+
+    // append filtered values to the list
+    filterdata.forEach(function(ufodata){
+        console.log(ufodata);
+        var row=tbody.append("tr");
+        Object.entries(ufodata).forEach(function([key, value]){
+            var cell=row.append("td");
+            cell.text(value);
+        });
+
+    });
+
+};
